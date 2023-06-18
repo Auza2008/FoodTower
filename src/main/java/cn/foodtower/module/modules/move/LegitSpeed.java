6dfc194e266f29a50a73ed90d6a96a6ffc954a70
Auza2008 <Auza2008@outlook.com>
2023-06-18 12:53:14 +0800

    LegitSpeed

---
+++ src/main/java/cn/foodtower/module/modules/move/LegitSpeed.java
@@ -0,0 +1,26 @@
+package cn.foodtower.module.modules.move;
+
+import cn.foodtower.api.EventHandler;
+import cn.foodtower.api.events.World.EventJump;
+import cn.foodtower.module.Module;
+import cn.foodtower.module.ModuleType;
+import cn.foodtower.util.entity.MoveUtils;
+import net.minecraft.potion.Potion;
+
+public class LegitSpeed extends Module {
+    public LegitSpeed() {
+        super("LegitSpeed", null, ModuleType.Movement);
+    }
+
+    @EventHandler
+    private void onJump(EventJump jump) {
+        if (mc.thePlayer != null || MoveUtils.isMoving()) {
+            if (mc.thePlayer.isPotionActive(Potion.moveSpeed) && mc.thePlayer.getActivePotionEffect(Potion.moveSpeed).getAmplifier() != 2) {
+                mc.thePlayer.motionX *= (0.7 * (mc.thePlayer.getActivePotionEffect(
+                        Potion.moveSpeed).getAmplifier() + 1));
+                mc.thePlayer.motionZ *= (0.7 * (mc.thePlayer.getActivePotionEffect(
+                        Potion.moveSpeed).getAmplifier() + 1));
+            }
+        }
+    }
+}
